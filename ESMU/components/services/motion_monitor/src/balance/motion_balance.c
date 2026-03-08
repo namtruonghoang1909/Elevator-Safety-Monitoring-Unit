234352c105803b639662b976f80b0ef1a13a9952
namtruonghoang1909 <nam.truonghoang05@gmail.com>
2026-03-08 13:30:27 +0700

    refactor(motion_monitor): remove legacy FSM implementation and add balance detection and filtering logic

---
+++ ESMU/components/services/motion_monitor/src/balance/motion_balance.c
@@ -0,0 +1,17 @@
+#include "motion_balance.h"
+
+balance_state_t mm_balance_update(float accel_x_g, float accel_y_g, float threshold_g) {
+    if (accel_x_g < -threshold_g) {
+        return BALANCE_STATE_TILT_LEFT;
+    } else if (accel_x_g > threshold_g) {
+        return BALANCE_STATE_TILT_RIGHT;
+    }
+
+    if (accel_y_g > threshold_g) {
+        return BALANCE_STATE_TILT_FORWARD;
+    } else if (accel_y_g < -threshold_g) {
+        return BALANCE_STATE_TILT_BACKWARD;
+    }
+
+    return BALANCE_STATE_LEVEL;
+}
